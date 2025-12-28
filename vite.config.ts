cea6b031bd94d5ef79b47bc14893db72884c6a36
jeffreybernadas <jeffrey.bernadas0@gmail.com>
2025-12-28 19:54:39 +0800

    feat(init): initialize project

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { keycloakify } from 'keycloakify/vite-plugin';
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    keycloakify({
+      accountThemeImplementation: 'none',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
